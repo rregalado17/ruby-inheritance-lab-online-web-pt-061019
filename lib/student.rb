7506fb06f12e0c98adebb14e809de8c3a539966a
Robinson Regalado <rregalado17@gmail.com>
2019-07-29 16:20:35 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+  attr_accessor :knowledge
+
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(new)
+    @knowledge << new 
+  end
+  
 end
